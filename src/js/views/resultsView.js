876e3f32d25a6ba09727dff900f8f08fdcb40e5b
Minh-Phuc Nguyen <mphncs@gmail.com>
2023-08-02 00:48:39 +0700

    refactor preview view

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,37 +1,12 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = 'No recipes found for your query. Please try again ;)';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(recipe) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          recipe.id === id ? 'preview__link--active' : ''
-        }" href="#${recipe.id}">
-          <figure class="preview__fig">
-            <img src=${recipe.image} alt=${recipe.title} />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${recipe.title}</h4>
-            <p class="preview__publisher">${recipe.publisher}</p>
-            <div class="preview__user-generated">
-              <svg>
-                <use href="${icons}#icon-user"></use>
-              </svg>
-            </div>
-          </div>
-        </a>
-      </li>
-    `;
+    return previewView.generateMarkup(this._data);
   }
 }
 
